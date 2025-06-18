4b76d81057e95bdbe092a3bc8c595c7fba26d52d
Sucy <hybaek807@naver.com>
2025-06-18 17:38:02 +0900

    feat: Enable custom index.html and crop-to-bottom piano UI workflow
    - Custom index.html in virtual_piano/src/templates now used via template_folder
    - Piano position plugin: crop, render, and gesture logic refactored for bottom-aligned UI
    - run_virtual_piano.py updated for new framework interface
    - Adds groundwork for sheet music drag-and-drop zone in cropped view

--- src/run_virtual_piano.py
+++ src/run_virtual_piano.py
@@ -3,6 +3,7 @@ eventlet.monkey_patch()
 
 from mediawebcore.core import run_server
 from piano_position_plugin import PianoPositionPlugin
+import os
 
 plugin = PianoPositionPlugin()
 
@@ -11,7 +12,8 @@ run_server(
     video_size_input=["80%", "40%"],
     video_size_output=["80%", "40%"],
     layout="top-bottom",
-
+    template="index.html",
+    template_folder=os.path.join(os.path.dirname(__file__), "templates"),
     audio_send=False,
     audio_receive=False
 )
